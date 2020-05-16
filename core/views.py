e79c6061fdd1c1ff3fa0229b28d226ef904d3616
Carlos Marcel <61261247+carlokinho80@users.noreply.github.com>
2020-05-15 23:10:56 -0300

    Criando um Agendamento
    Signed-off-by: carlokinho80 <ca_marcel@hotmail.com>

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from core.models import Evento
+
 
 # Create your views here.
+
+def lista_eventos(resquest):
+    evento = Evento.objects.all()
+    response = {'eventos':evento}
+    return render(resquest,'agenda.html', response)
